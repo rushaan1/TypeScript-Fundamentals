b935178d132a4ea6346b31c4bf65c3958aa0d929
rushaan1 <cocrushaan3@gmail.com>
2024-09-19 22:13:17 +0530

    Added Interface fundamental related code

--- TypeScriptFundamentals/app.ts
+++ TypeScriptFundamentals/app.ts
@@ -27,7 +27,42 @@ const members: (User | admin)[] = [];
 //tuples
 let samepleTuple: [string, string, boolean, number];
 let rgb: [number, number, number] = [0, 0, 255];
-// In a tuple the elements are stored in the exact same order and amount of the types mentioned 
+// In a tuple the elements are stored in the exact same order and amount of the types mentioned
+
+interface SubscriberInterface
+{
+    readonly dbId: number,
+    email: string,
+    userid: number,
+    googleid: string,
+    startTrial(): string,
+    getCoupon(couponname: string, value: number): number
+};
+
+interface SubscriberInterface
+{
+    githubToken: string
+}; //Adding a new property to existing interface
+
+interface SubscriberRoleInterface extends SubscriberInterface
+{
+    role: "admin" | "ta" | "learner"
+}
+
+const hitesh: SubscriberRoleInterface = {
+    dbId: 22, email: "h@h.com", userid: 2211,
+    role: "admin",
+    githubToken: "github",
+    googleid: "samplegoogleid",
+    startTrial: () => {
+        return "trial started"
+    },
+    getCoupon: (name: string, off: number) => {
+        return 10
+    }
+};
+hitesh.email = "h@hc.com";
+
 
 function createUser(user: User): boolean
 {
